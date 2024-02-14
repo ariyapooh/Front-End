a7e3473084c75ede7e438d514b8b85bf34dc20e0
ariyapooh <s6406021621137@fitm.kmutnb.ac.th>
2024-02-14 10:26:09 +0700

    save1

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 //const base_url = "http://localhost:3000";
 const base_url = "http://node58395-env-4744631.proen.app.ruk-com.cloud"
 
-app.set("views", path.join(__dirname, "/public/Views"));
+app.set("views", path.join(__dirname, "/public/views"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
